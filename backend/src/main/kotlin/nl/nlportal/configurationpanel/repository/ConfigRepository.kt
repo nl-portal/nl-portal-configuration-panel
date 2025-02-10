b042151fa413f06778fd9bc130c5d454db7a5184
Anna Vissart <anna.vissart@ritense.com>
2025-02-10 11:30:58 +0100

    * update repository and entity * add default value to cache ttl * use plain postgres image * update tests

--- backend/src/main/kotlin/nl/nlportal/configurationpanel/repository/ConfigRepository.kt
+++ backend/src/main/kotlin/nl/nlportal/configurationpanel/repository/ConfigRepository.kt
@@ -1,9 +1,16 @@
 package nl.nlportal.configurationpanel.repository
 
-import nl.nlportal.configurationpanel.domain.Config
+import nl.nlportal.configurationpanel.domain.ConfigurationProperty
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface ConfigRepository: JpaRepository<Config, String> {
+interface ConfigRepository : JpaRepository<ConfigurationProperty, String> {
 
-     fun findByFeatureId(featureId: String): Config?
+    fun findByApplication(featureId: String): List<ConfigurationProperty>?
+
+    fun findByApplicationAndPropertyKey(application: String, propertyKey: String): ConfigurationProperty?
+
+    fun findByApplicationAndPropertyKeyStartsWith(
+        application: String,
+        propertyKeyPrefix: String
+    ): List<ConfigurationProperty>?
 }
