b8521bf86431b59b7466fd3eec05c5ad35bd3123
Rick Veenstra <rick.veenstra@amsterdam.nl>
2025-02-18 10:41:19 +0100

    feat: Made the jwt issuer uri an environment variable

--- frontend/public/config.js
+++ frontend/public/config.js
@@ -1,4 +1,4 @@
 window.OIDC_URL = 'http://localhost:8082/auth/realms/nlportalconfig';
 window.OIDC_REALM = 'nlportalconfig';
 window.OIDC_CLIENT_ID = 'nl-portal-config';
-window.OIDC_REDIRECT_URI = 'http://localhost:5173/keycloak/callback';
+window.OIDC_REDIRECT_URI = 'http://localhost:3001/keycloak/callback';
