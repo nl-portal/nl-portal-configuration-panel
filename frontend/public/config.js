d180c43bf8479eda3c42a22c95f8a4f322cb2b60
Anna Vissart <anna.vissart@ritense.com>
2025-08-08 12:10:05 +0200

    * change port on default fe config

--- frontend/public/config.js
+++ frontend/public/config.js
@@ -17,6 +17,6 @@
 window.OIDC_URL='http://localhost:8082/auth/realms/nlportalconfig';
 window.OIDC_REALM='nlportalconfig';
 window.OIDC_CLIENT_ID='nl-portal-config';
-window.OIDC_REDIRECT_URI='http://localhost:3333';
-window.OIDC_POST_LOGOUT_REDIRECT_URI='http://localhost:3333';
+window.OIDC_REDIRECT_URI='http://localhost:3001';
+window.OIDC_POST_LOGOUT_REDIRECT_URI='http://localhost:3001';
 window.CLIENT_APPLICATION_NAME='nl-portal-app';
